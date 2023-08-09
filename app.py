34702e86a9cffe7178673ea8028241a41de0ed1f
Nguyen Viet Anh <93577916+vanhanh210@users.noreply.github.com>
2023-08-09 11:46:47 +0700

    Update

--- app.py
+++ app.py
@@ -12,17 +12,20 @@ shorten_url = st.sidebar.checkbox('Use shortlink (TinyURL)?')
 # Input for the user to enter the URL
 url = st.text_input('Enter the URL you want to convert to QR Code:')
 
-if url and shorten_url:
-    # Shorten the URL using TinyURL
-    s = pyshorteners.Shortener()
-    url = s.tinyurl.short(url)
-    st.write('Shortened URL:', url)
+# Upload logo
+logo_file = st.file_uploader('Upload your logo (optional):')
 
 if url:
+    if shorten_url:
+        # Shorten the URL using TinyURL
+        s = pyshorteners.Shortener()
+        url = s.tinyurl.short(url)
+        st.write('Shortened URL:', url)
+
     # Create a QR Code instance
     qr = qrcode.QRCode(
         version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
         border=4,
     )
@@ -32,7 +35,16 @@ if url:
     qr.make(fit=True)
 
     # Create an Image object from the QR Code instance
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+
+    # Add the logo if uploaded
+    if logo_file:
+        logo = Image.open(logo_file)
+        logo = logo.resize((50, 50))  # Adjust size to fit inside QR code
+        img_w, img_h = img.size
+        logo_w, logo_h = logo.size
+        offset = ((img_w - logo_w) // 2, (img_h - logo_h) // 2)
+        img.paste(logo, offset)
 
     # Save the image to a BytesIO object
     buffer = io.BytesIO()
