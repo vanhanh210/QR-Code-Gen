971c5015418cc3fbff76bdfbb3c197b3c2f2b3c6
Nguyen Viet Anh <93577916+vanhanh210@users.noreply.github.com>
2023-08-09 12:25:57 +0700

    Update Ver 2.0

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import qrcode
 from PIL import Image
 import io
 import pyshorteners
+import base64
 
 st.title('QR Code Generator with Logo')
 
@@ -15,6 +16,11 @@ url = st.text_input('Enter the URL you want to convert to QR Code:')
 # Option to upload a logo
 uploaded_logo = st.file_uploader('Upload a logo (optional):')
 
+# Options to select quality and format
+quality = st.sidebar.slider('Select Quality (DPI):', min_value=72, max_value=300, value=150, step=1)
+format_options = st.sidebar.selectbox('Select Image Format:', options=['PNG', 'JPEG', 'BMP'])
+format_extension = format_options.lower()
+
 if url and shorten_url:
     # Shorten the URL using TinyURL
     s = pyshorteners.Shortener()
@@ -24,7 +30,7 @@ if url and shorten_url:
 if url:
     # Create a QR Code instance
     qr = qrcode.QRCode(
-        version=6, # You might need to increase this if the logo interferes with the code
+        version=6,
         error_correction=qrcode.constants.ERROR_CORRECT_Q,
         box_size=10,
         border=4,
@@ -40,11 +46,11 @@ if url:
     # If a logo is uploaded, embed it in the center of the QR code
     if uploaded_logo:
         logo = Image.open(uploaded_logo).convert('RGBA')
-        logo_size = 80  # Increased size of the logo
+        logo_size = 80
         logo = logo.resize((logo_size, logo_size), Image.ANTIALIAS)
         qr_size = img.size[0]
         logo_position = ((qr_size - logo_size) // 2, (qr_size - logo_size) // 2)
-        
+
         # Create a white box in the center of QR code where the logo will be placed
         for x in range(logo_position[0], logo_position[0] + logo_size):
             for y in range(logo_position[1], logo_position[1] + logo_size):
@@ -55,8 +61,14 @@ if url:
 
     # Save the image to a BytesIO object
     buffer = io.BytesIO()
-    img.save(buffer, format="PNG")
+    img.save(buffer, format=format_extension, dpi=(quality, quality))
     buffer.seek(0)
 
     # Display the image
     st.image(buffer, caption='Generated QR Code', use_column_width=True)
+
+    # Download Button
+    buffer.seek(0)
+    b64 = base64.b64encode(buffer.read()).decode()
+    download_button = f'<a href="data:image/{format_extension};base64,{b64}" download="qr_code.{format_extension}">Download QR Code</a>'
+    st.markdown(download_button, unsafe_allow_html=True)
