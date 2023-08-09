43907521e2fa1bd30fd3c4b533bb31d0cde6ff9a
Nguyen Viet Anh <93577916+vanhanh210@users.noreply.github.com>
2023-08-09 12:10:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import qrcode
 from PIL import Image
+import qrcode
 import io
 import pyshorteners
 
@@ -22,37 +22,31 @@ if url:
         url = s.tinyurl.short(url)
         st.write('Shortened URL:', url)
 
-    # Create a QR Code instance
+    # Generate the QR code
     qr = qrcode.QRCode(
-        version=6,  # Choose an appropriate version to fit the logo
+        version=6,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
+        box_size=6,
         border=4,
     )
-
-    # Add the URL
     qr.add_data(url)
     qr.make(fit=True)
+    qr_img = qr.make_image(fill='black', back_color='white').convert('RGB')
 
-    # Create an Image object from the QR Code instance
-    img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
-    img_w, img_h = img.size
-
-    # Create a blank space for the logo if uploaded
+    # Place logo in the center if uploaded
     if logo_file:
-        logo_size = 50  # Adjust as needed
-        offset = ((img_w - logo_size) // 2, (img_h - logo_size) // 2)
-        for y in range(offset[1], offset[1] + logo_size):
-            for x in range(offset[0], offset[0] + logo_size):
-                img.putpixel((x, y), (255, 255, 255))
-
         logo = Image.open(logo_file)
-        logo = logo.resize((logo_size, logo_size))  # Adjust size to fit inside QR code
-        img.paste(logo, offset)
+        logo = logo.convert("RGBA")
+        logo_size = 100 # Adjust logo size
+        logo = logo.resize((logo_size, logo_size), Image.ANTIALIAS)
+        logo_w, logo_h = logo.size
+        qr_img_w, qr_img_h = qr_img.size
+        offset = ((qr_img_w - logo_w) // 2, (qr_img_h - logo_h) // 2)
+        qr_img.paste(logo, offset, logo) # logo as the mask parameter to handle transparency
 
     # Save the image to a BytesIO object
     buffer = io.BytesIO()
-    img.save(buffer, format="PNG")
+    qr_img.save(buffer, format="PNG")
     buffer.seek(0)
 
     # Display the image
